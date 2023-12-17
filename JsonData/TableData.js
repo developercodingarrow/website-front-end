02d3fe62daabb881b80b6f7099e037cf7eff74ed
developercodingarrow <digitalcloudinfo@gmail.com>
2023-12-17 21:58:13 +0530

    implement date fillter

--- JsonData/TableData.js
+++ JsonData/TableData.js
@@ -6,57 +6,57 @@ export const newUserColumn = [
   { label: "Mobile Number", key: "mobileNumber", component: "text" },
 ];
 
-// export const sampleData = [
-//   {
-//     _id: "1",
-//     name: "sanjay",
-//     email: "sanjay@gmail.com",
-//     mobileNumber: "9992118210",
-//     date: "20-12-2023",
-//   },
-//   {
-//     _id: "2",
-//     name: "sanjay",
-//     email: "sanjay@gmail.com",
-//     mobileNumber: "9992118210",
-//     date: "20-12-2023",
-//   },
-//   {
-//     _id: "3",
-//     name: "sanjay",
-//     email: "sanjay@gmail.com",
-//     mobileNumber: "9992118210",
-//     date: "20-12-2023",
-//   },
-//   {
-//     _id: "4",
-//     name: "sanjay",
-//     email: "sanjay@gmail.com",
-//     mobileNumber: "9992118210",
-//     date: "20-12-2023",
-//   },
-//   {
-//     _id: "5",
-//     name: "sanjay",
-//     email: "sanjay@gmail.com",
-//     mobileNumber: "9992118210",
-//     date: "20-12-2023",
-//   },
-//   {
-//     _id: "6",
-//     name: "sanjay",
-//     email: "sanjay@gmail.com",
-//     mobileNumber: "9992118210",
-//     date: "20-12-2023",
-//   },
-//   {
-//     _id: "7",
-//     name: "sanjay",
-//     email: "sanjay@gmail.com",
-//     mobileNumber: "9992118210",
-//     date: "20-12-2023",
-//   },
-// ];
+export const sampleData = [
+  {
+    _id: "1",
+    name: "sanjay",
+    email: "sanjay@gmail.com",
+    mobileNumber: "9992118210",
+    date: "2023-12-20",
+  },
+  {
+    _id: "2",
+    name: "sanjay",
+    email: "sanjay@gmail.com",
+    mobileNumber: "9992118210",
+    date: "2023-12-21",
+  },
+  {
+    _id: "3",
+    name: "sanjay",
+    email: "sanjay@gmail.com",
+    mobileNumber: "9992118210",
+    date: "2023-12-22",
+  },
+  {
+    _id: "4",
+    name: "sanjay",
+    email: "sanjay@gmail.com",
+    mobileNumber: "9992118210",
+    date: "2023-12-23",
+  },
+  {
+    _id: "5",
+    name: "sanjay",
+    email: "sanjay@gmail.com",
+    mobileNumber: "9992118210",
+    date: "2023-12-24",
+  },
+  {
+    _id: "6",
+    name: "sanjay",
+    email: "sanjay@gmail.com",
+    mobileNumber: "9992118210",
+    date: "2023-12-25",
+  },
+  {
+    _id: "7",
+    name: "sanjay",
+    email: "sanjay@gmail.com",
+    mobileNumber: "9992118210",
+    date: "2023-12-26",
+  },
+];
 
 // export const sampleData = Array.from({ length: 100 }, (_, index) => ({
 //   _id: (index + 1).toString(),
@@ -66,10 +66,10 @@ export const newUserColumn = [
 //   date: "20-12-2023",
 // }));
 
-export const sampleData = Array.from({ length: 100 }, (_, index) => ({
-  _id: (index + 1).toString(),
-  username: `user${index + 1}`,
-  email: `user${index + 1}@example.com`,
-  mobileNumber: "9992118210",
-  date: `2${(index % 9) + 1}-12-2023`, // Varying the day part of the date
-}));
+// export const sampleData = Array.from({ length: 100 }, (_, index) => ({
+//   _id: (index + 1).toString(),
+//   username: `user${index + 1}`,
+//   email: `user${index + 1}@example.com`,
+//   mobileNumber: "9992118210",
+//   date: `2${(index % 9) + 1}-12-2023`, // Varying the day part of the date
+// }));
--- Utils/Component-logics/dateRangeFilter.js
+++ Utils/Component-logics/dateRangeFilter.js
@@ -2,8 +2,10 @@
 
 export const filterByDateRange = (data, startDate, endDate) => {
   console.log(data);
+
   const start = new Date(startDate);
   const end = new Date(endDate);
+  console.log(start, end);
   const filteredData = data.filter((item) => {
     const itemDate = new Date(item.date);
     console.log(itemDate);
